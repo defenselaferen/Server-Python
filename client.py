a2d290a93fad7dec298a428ff91bd51f6a36fb9f
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-06-11 20:46:26 +0700

    Make File Client
    The New File

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.26"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Client New")
+input()
+send("Hello Every One And You Too!")
+input()
+send("Hello Github.com and Socket")
+
+send(DISCONNECT_MESSAGE)
